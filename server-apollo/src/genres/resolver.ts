a8166c192093f10576543a634f3867be23fd4de4
nassim <nassim.boudmea@etu.u-bordeaux.fr>
2025-04-29 16:43:19 +0200

    Exercice partie server

---
+++ server-apollo/src/genres/resolver.ts
@@ -0,0 +1,18 @@
+import { Resolvers } from "../types";
+
+export const songResolvers: Resolvers = {
+  Query: {
+    songs: (_, __, { dataSources }) => {
+      return dataSources.db.song.findMany();
+    },
+    songById: (_, { id }, { dataSources }) => {
+        return dataSources.db.song.findById(id);
+    }
+  },
+
+  Song: {
+    user: async (parent, _, { dataSources }) => {
+      return dataSources.db.user.findById(parent.userId);
+    },
+  },
+};
